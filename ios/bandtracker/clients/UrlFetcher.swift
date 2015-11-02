d8bd732cdeb6df44e1da2ccb40e4c1d5110210c8
Johan Smet <johan.smet@justcode.be>
2015-11-02 22:11:15 +0100

    UrlFetcher : try to force using images from cache

--- ios/bandtracker/clients/UrlFetcher.swift
+++ ios/bandtracker/clients/UrlFetcher.swift
@@ -10,12 +10,28 @@ import Foundation
 import UIKit
 
 class UrlFetcher {
+    
+    static var urlSession : NSURLSession?
    
     static func loadImageFromUrl(urlString : String, completionHandlerUI : (image : UIImage?) -> Void) {
         
-        guard let url  = NSURL(string: urlString) else { return }
+        // create session if necessary
+        if (urlSession == nil) {
+            let cache = NSURLCache(memoryCapacity: 64 * 1024 * 1024, diskCapacity: 512 * 1024 * 1024, diskPath: "img-cache")
+            
+            let sessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
+            sessionConfiguration.URLCache = cache
+            sessionConfiguration.requestCachePolicy = .ReturnCacheDataElseLoad;
+            
+            urlSession = NSURLSession(configuration: sessionConfiguration)
+        }
         
-        let task = NSURLSession.sharedSession().dataTaskWithRequest(NSURLRequest(URL: url)) { data, response, urlError in
+        // configure request
+        guard let url  = NSURL(string: urlString) else { return }
+        let request = NSURLRequest(URL: url)
+        
+        // execute request
+        let task = urlSession!.dataTaskWithRequest(request) { data, response, urlError in
             if let data = data {
                 dispatch_async(dispatch_get_main_queue()) {
                     completionHandlerUI(image: UIImage(data: data))
