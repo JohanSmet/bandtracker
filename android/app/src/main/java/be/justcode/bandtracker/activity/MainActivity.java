27b1a036b2331e2aec45d53a40126f7e9dcc01bd
Johan Smet <johan.smet@justcode.be>
2016-01-01 22:48:31 +0100

    android : add toolbar to main activity

--- android/app/src/main/java/be/justcode/bandtracker/activity/MainActivity.java
+++ android/app/src/main/java/be/justcode/bandtracker/activity/MainActivity.java
@@ -1,7 +1,10 @@
 package be.justcode.bandtracker.activity;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import be.justcode.bandtracker.R;
 
@@ -13,5 +16,28 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // toolbar
+        Toolbar toolBar = (Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(toolBar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_main_toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.action_band_add:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
