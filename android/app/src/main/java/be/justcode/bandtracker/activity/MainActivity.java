423c068e324c05f4007805268226c1e8db4851b3
Johan Smet <johan.smet@justcode.be>
2016-04-09 21:29:58 +0200

    android : reworked main activity to a tabbed view with a recyclerview and floating action button

--- android/app/src/main/java/be/justcode/bandtracker/activity/MainActivity.java
+++ android/app/src/main/java/be/justcode/bandtracker/activity/MainActivity.java
@@ -1,27 +1,16 @@
 package be.justcode.bandtracker.activity;
 
-import android.content.Context;
-import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.RatingBar;
-import android.widget.TextView;
 
 import be.justcode.bandtracker.R;
 import be.justcode.bandtracker.clients.DataLoader;
-import be.justcode.bandtracker.model.Band;
 import be.justcode.bandtracker.model.DataContext;
-import be.justcode.bandtracker.utils.BandImageDownloader;
-import be.justcode.bandtracker.utils.FlowCursorAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,54 +25,13 @@ public class MainActivity extends AppCompatActivity {
         // data sync
         DataLoader.downloadDataAsync(getApplicationContext());
 
-        // toolbar
-        Toolbar toolBar = (Toolbar) findViewById(R.id.toolBar);
-        setSupportActionBar(toolBar);
+        // viewpager
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager.setAdapter(new MainFragmentPagerAdapter(getSupportFragmentManager()));
 
-        // list view
-        final ListView listView = (ListView) findViewById(R.id.listMainBands);
-
-        mListAdapter = new BandsSeenAdapter(this, "");
-        listView.setAdapter(mListAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                BandDetailsActivity.showBand(MainActivity.this, mListAdapter.getItem(position));
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main_toolbar, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId())
-        {
-            case R.id.action_band_add:
-                Intent intent = new Intent(this, BandSearchActivity.class);
-                startActivityForResult(intent, BandSearchActivity.SELECT_BAND_REQUEST);
-                break;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == BandSearchActivity.SELECT_BAND_REQUEST && resultCode == RESULT_OK) {
-            String bandId = data.getStringExtra("bandId");
-            refreshData();
-        }
-    }
-
-    private void refreshData() {
-        mListAdapter.changePattern("");
+        // tablayout
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabContainer);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -91,63 +39,31 @@ public class MainActivity extends AppCompatActivity {
     // nested classes
     //
 
-    private class BandsSeenAdapter extends FlowCursorAdapter<Band> {
-        public BandsSeenAdapter(Context context, String pattern) {
-            mContext = context;
-            changeCursor(DataContext.bandCursor(pattern));
+    private class MainFragmentPagerAdapter extends FragmentPagerAdapter {
+
+        public MainFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
         }
 
-        public void changePattern(String pattern) {
-            changeCursor(DataContext.bandCursor(pattern));
+        @Override
+        public int getCount() {
+            return pageNames.length;
         }
 
-        public View newView(ViewGroup parent) {
-            View view = LayoutInflater.from(mContext).inflate(R.layout.row_bands_seen, parent, false);
-
-            ViewHolder holder = new ViewHolder();
-            holder.lblBandName = (TextView) view.findViewById(R.id.lblBandName);
-            holder.lblNumGigs  = (TextView) view.findViewById(R.id.lblNumGigs);
-            holder.imgBand     = (ImageView) view.findViewById(R.id.imgBand);
-            holder.imgLogo     = (ImageView) view.findViewById(R.id.imgLogo);
-            holder.ratingBar   = (RatingBar) view.findViewById(R.id.ratingBar);
-            view.setTag(holder);
-
-            return view;
+        @Override
+        public Fragment getItem(int position) {
+            return pages[position];
         }
 
-        public void bindView(View view, int position) {
-
-            Band band = getItem(position);
-            ViewHolder holder = (ViewHolder) view.getTag();
-
-            if (!holder.lblBandName.getText().equals(band.getName())) {
-                holder.lblBandName.setText(band.getName());
-                BandImageDownloader.thumbnail(band, MainActivity.this, holder.imgBand);
-                BandImageDownloader.logo(band, MainActivity.this, holder.imgLogo);
-            }
-
-            holder.ratingBar.setRating((float) band.getAvgRating() / 10.0f);
-
-            int textId = (band.getNumGigs() == 0) ? R.string.band_list_numgigs_none : (band.getNumGigs() == 1 ) ? R.string.band_list_numgigs_single : R.string.band_list_numgigs_multiple;
-            holder.lblNumGigs.setText(String.format(getString(textId), band.getNumGigs()));
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return pageNames[position];
         }
 
-        private class ViewHolder {
-            TextView    lblBandName;
-            TextView    lblNumGigs;
-            RatingBar   ratingBar;
-            ImageView   imgBand;
-            ImageView   imgLogo;
-        }
-
-        private Context              mContext;
+        private Fragment[]      pages = {MainBandsFragment.newInstance()};
+        private CharSequence[]  pageNames = {getText(R.string.page_bands)};
     }
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    //
-    // member variables
-    //
 
-    private BandsSeenAdapter mListAdapter;
 }
 
---
+++ android/app/src/main/java/be/justcode/bandtracker/activity/MainBandsFragment.java
@@ -0,0 +1,170 @@
+package be.justcode.bandtracker.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.raizlabs.android.dbflow.list.FlowCursorList;
+
+import be.justcode.bandtracker.App;
+import be.justcode.bandtracker.R;
+import be.justcode.bandtracker.model.Band;
+import be.justcode.bandtracker.model.DataContext;
+import be.justcode.bandtracker.utils.BandImageDownloader;
+
+public class MainBandsFragment extends Fragment {
+
+    public static MainBandsFragment newInstance() {
+        MainBandsFragment fragment = new MainBandsFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main_bands, container, false);
+
+        // setup recycler view
+        final RecyclerView rvBands = (RecyclerView) view.findViewById(R.id.listMainBands);
+
+        mListAdapter = new BandsSeenAdapter("");
+        rvBands.setAdapter(mListAdapter);
+        rvBands.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rvBands.setHasFixedSize(true);
+
+
+        // add band button
+        final FloatingActionButton btnBandAdd = (FloatingActionButton) view.findViewById(R.id.btnBandAdd);
+        btnBandAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), BandSearchActivity.class);
+                startActivityForResult(intent, BandSearchActivity.SELECT_BAND_REQUEST);
+            }
+        });
+
+
+        return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == BandSearchActivity.SELECT_BAND_REQUEST && resultCode == Activity.RESULT_OK) {
+            String bandId = data.getStringExtra("bandId");
+            mListAdapter.refresh();
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    // nested classes
+    //
+
+    private class BandsSeenAdapter extends RecyclerView.Adapter<BandsSeenAdapter.ViewHolder> {
+
+        public BandsSeenAdapter(String pattern) {
+            mCursor = DataContext.bandCursor(pattern);
+        }
+
+        @Override
+        public BandsSeenAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+
+            // inflate layout
+            View rowView = inflater.inflate(R.layout.row_bands_seen, parent, false);
+
+            // create holder
+            return new ViewHolder(rowView);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            Band band = mCursor.getItem(position);
+
+            if (!holder.lblBandName.getText().equals(band.getName())) {
+                holder.lblBandName.setText(band.getName());
+                BandImageDownloader.thumbnail(band, App.getContext(), holder.imgBand);
+                BandImageDownloader.logo(band, App.getContext() , holder.imgLogo);
+            }
+
+            holder.ratingBar.setRating((float) band.getAvgRating() / 10.0f);
+
+            int textId = (band.getNumGigs() == 0) ? R.string.band_list_numgigs_none : (band.getNumGigs() == 1 ) ? R.string.band_list_numgigs_single : R.string.band_list_numgigs_multiple;
+            holder.lblNumGigs.setText(String.format(getString(textId), band.getNumGigs()));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCursor.getCount();
+        }
+
+        public void refresh() {
+            mCursor = DataContext.bandCursor("");
+            notifyDataSetChanged();
+        }
+
+        public void rowClicked(int position) {
+            BandDetailsActivity.showBand(getActivity(), mCursor.getItem(position));
+        }
+
+        // view holder
+        public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+            public ViewHolder(View view) {
+                super(view);
+
+                lblBandName = (TextView) view.findViewById(R.id.lblBandName);
+                lblNumGigs  = (TextView) view.findViewById(R.id.lblNumGigs);
+                imgBand     = (ImageView) view.findViewById(R.id.imgBand);
+                imgLogo     = (ImageView) view.findViewById(R.id.imgLogo);
+                ratingBar   = (RatingBar) view.findViewById(R.id.ratingBar);
+
+                view.setOnClickListener(this);
+            }
+
+            @Override
+            public void onClick(View v) {
+                rowClicked(getAdapterPosition());
+            }
+
+            // member variables
+            TextView    lblBandName;
+            TextView    lblNumGigs;
+            RatingBar   ratingBar;
+            ImageView   imgBand;
+            ImageView   imgLogo;
+        }
+
+        // member variables
+        private FlowCursorList<Band> mCursor;
+
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    // member variables
+    //
+
+    private BandsSeenAdapter mListAdapter;
+
+
+
+}
